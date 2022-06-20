27c2cd63eda88a74ac5976ec926fd23df16bb75a
AndreiVelmezev <velmezev@gmail.com>
2022-06-20 07:43:18 +0300

    dz

---
+++ src/Dz1d9/Main.java
@@ -0,0 +1,17 @@
+package Dz1d9;
+
+public class Main {
+    public static void main(String[] args) {
+        Author author1 = new Author("Lev","Tolstoy");
+        Book book1=new Book("Voyna i mir", author1,1865);
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+
+        book1.setPublishingYear(1869);
+
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+    }
+
+
+}
